7f399389ef17d5c938dd0c3d8a62c64d04ba0c96
David Vo <datcv@hotmail.com>
2020-04-21 23:33:33 +1000

    checking guesses

--- src/App.js
+++ src/App.js
@@ -5,51 +5,52 @@ import "./App.css";
 
 const App = () => {
   const [levelArr, setLevelArr] = useState([]);
-  const [left, setLeft] = useState({});
-  const [right, setRight] = useState({});
+  
+  const [currentLevel, setCurrentLevel] = useState(null);
+
+  const [matchedIndexes, setMatchedIndexes] = useState([]);
+
+  const [leftCanvasContext, setLeftCanvasContext] = useState(null);
+  const [rightCanvasContext, setRightCanvasContext] = useState(null);
+  const [guessWrong, setGuessWrong] = useState(false);
+  
+  const leftCanvas = useRef(null);
+  const rightCanvas = useRef(null);
+  const leftImage = useRef(null);
+  const rightImage = useRef(null);
+
+  
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
-  const [second, setSecond] = useState(120);
-
-  const [canvasContext, setCanvasContext] = useState(null)
-  const canvas = useRef(null);
-  const leftImage = useRef(null);
   
-  // const chartPositionData = [
-  //   {"x": 560 ,"y": 181.8125},
-  //   {"x": 481 ,"y": 215.8125},
-  //   {"x": 88  ,"y": 50.8125},
-  //   {"x": 85  ,"y": 267.8125},
-  //   {"x": 426 ,"y": 359.8125},
-  //   {"x": 262 ,"y": 315.8125},
-  //   {"x": 456 ,"y": 550.8125},
-  //   {"x": 189 ,"y": 561.8125},
-  //   {"x": 43  ,"y": 469.8125},
-  //   {"x": 394 ,"Y": 39.8125}
-  // ];
-
   useEffect(() => {
-    const ctx = canvas.current.getContext("2d");
-    // canvas.current.width = 600;
-    // canvas.current.height = 600;
-
-    fitToContainer(canvas.current);
-    setCanvasContext(ctx);
+    fitToContainer(leftCanvas.current);
+    setLeftCanvasContext(leftCanvas.current.getContext("2d"));
+    fitToContainer(rightCanvas.current);
+    setRightCanvasContext(rightCanvas.current.getContext("2d"));
 
     db.ref("/levels")
       .once("value")
       .then((snapshot) => {
+        
         const levels = Object.entries(snapshot.val()).map((x) => x[1]);
+        
+        console.log(levels);
+
         setLevelArr(levels);
-        setLeft(levels[0]);
-        setRight(levels[0]);
+        setCurrentLevel(levels[0]);
+      
       });
 
       leftImage.current.onload = () => {
-        canvas.current.getContext("2d").drawImage(leftImage.current, 0, 0, canvas.current.width, canvas.current.height);
+        leftCanvas.current.getContext("2d").drawImage(leftImage.current, 0, 0, leftCanvas.current.width, leftCanvas.current.height);
       }
 
-    return () => {};
+      rightImage.current.onload = () => {
+        rightCanvas.current.getContext("2d").drawImage(rightImage.current, 0, 0, rightCanvas.current.width, rightCanvas.current.height);
+      }
+
+    return () => {}
   }, []);
 
   const fitToContainer = (canvas) => {
@@ -57,52 +58,104 @@ const App = () => {
     canvas.style.width ='100%';
     canvas.style.height='100%';
     // ...then set the internal size to match
-    canvas.width  = canvas.offsetWidth;
-    canvas.height = canvas.offsetHeight;
+    
+    canvas.width  = window.innerWidth / 2 - 20; //canvas.offsetWidth;
+    canvas.height = window.innerHeight - 100; //canvas.offsetHeight + 400;
+    
   }
 
   const relativeCoords = (event) => {
     var bounds = event.target.getBoundingClientRect();
-    var x1 = (event.clientX - bounds.left) / bounds.width * event.target.width;
-    var y2 = (event.clientY - bounds.top) / bounds.height * event.target.height;
+    
+    const left = (event.clientX - bounds.left) / bounds.width * event.target.width;
+    const top = (event.clientY - bounds.top) / bounds.height * event.target.height;
 
-    setX(x1);
-    setY(y2);
+    setX(left);
+    setY(top);
 
-    canvasContext.fill = 'green';
-    canvasContext.beginPath();
-    canvasContext.arc(x1, y2, 20, 0, 2 * Math.PI);
-    canvasContext.stroke();
+    if (Array.isArray(currentLevel.results))
+    {
+      const foundResult = currentLevel.results.find(r => isInRect(r, top, left));
+      if (foundResult !== undefined)
+      {
+          const foundIndex = currentLevel.results.indexOf(foundResult);
+          if (!matchedIndexes.includes(foundIndex))
+          {
+            setMatchedIndexes([...matchedIndexes, foundIndex]);
+            const rx = (foundResult.x1 - foundResult.x0)/2;
+            const ry = (foundResult.y1 - foundResult.y0)/2;
+            const cx  = foundResult.x0 + rx;
+            const cy  = foundResult.y0 + (foundResult.y1 - foundResult.y0)/2;
+            
+            
+
+            [leftCanvasContext, rightCanvasContext].forEach(ct => {
+              ct.strokeStyle = 'red';
+              ct.lineWidth  = 4;
+              ct.beginPath();
+              ct.ellipse(cx, cy, rx, ry, 0, 0, 2 * Math.PI, false);
+              ct.stroke();
+            });
+            
+          }
+      }
+      else{
+        setGuessWrong(true);
+      }
+    }
+
   };
+
+  const isInRect = (result, top, left) => {
+    const {x0, y0, x1, y1} = result;
+    if (top < y0 || top > y1 || left < x0 || left > x1)
+    {
+      return false;
+    }
+
+    return true;
+  }
+
+  const guessWorngHandler = () => {
+    if (guessWrong)
+    {
+        setGuessWrong(false);
+        return true;
+    }
+
+    return false;
+  }
 
   return (
     <div className="App">
       <div className="header">
-        <CountDownTimer second={second} />
+        <CountDownTimer second={120} guessWorngHandler={guessWorngHandler} />
         <span>X: {x} - Y:{y}</span>
       </div>
 
       <div className="container">
         <div className="left-half">
-          <canvas ref={canvas} onMouseDown={relativeCoords}></canvas>
+          <canvas ref={leftCanvas} onMouseDown={relativeCoords}></canvas>
+          <img
+            ref={leftImage}
+            className="hiden-Image"
+            src={(currentLevel && currentLevel.left) || "https://via.placeholder.com/400x300"}
+            alt="Uploaded Images"
+          />
         </div>
 
         <div className="right-half">
+          <canvas ref={rightCanvas} onMouseDown={relativeCoords}></canvas>
           <img
-            className="compare-image"
-            src={right.right || "https://via.placeholder.com/400x300"}
+            ref={rightImage}
+            className="hiden-Image"
+            src={(currentLevel && currentLevel.right) || "https://via.placeholder.com/400x300"}
             alt="Uploaded Images"
-            onMouseDown={relativeCoords}
           />
         </div>
       </div>
 
-      <img
-        ref={leftImage}
-        className="compare-image"
-        src={left.left || "https://via.placeholder.com/400x300"}
-        alt="Uploaded Images"
-      />
+     
     </div>
   );
 };
--- src/Components/CountDownTimer.js
+++ src/Components/CountDownTimer.js
@@ -1,23 +1,41 @@
 import React, { useState, useEffect } from "react";
 
-export const CountDownTimer = ({ second }) => {
+export const CountDownTimer = ({ second, guessWorngHandler }) => {
   const [counter, setCounter] = useState(second);
+  
+  const min = Math.floor((counter % 3600) / 60);
+  const sec = Math.floor((counter % 3600) % 60);
+  const milliSec = (counter % 1) * 100;
 
-  let min = Math.floor((counter % 3600) / 60);
-  let sec = Math.floor((counter % 3600) % 60);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const isGuessWrong = () => {
+    return guessWorngHandler();
+  }
 
   useEffect(() => {
-    const timer =
-      counter > 0 && setInterval(() => setCounter(counter - 1), 1000);
-    return () => clearInterval(timer);
-  }, [counter]);
+    const timer = counter > 0 && setInterval(() => {
+      let newCounter = counter - 0.02;
+      if (isGuessWrong())
+      {
+        newCounter -= 5;
+      }
 
-  const formatTime = (n) =>
-    n.toLocaleString("en-US", { minimumIntegerDigits: 2, useGrouping: false });
+      if (newCounter < 0)
+      {
+        newCounter = 0;
+      }
+
+      setCounter(newCounter);
+    }, 20);
+    return () => clearInterval(timer);
+  }, [counter, isGuessWrong]);
+
+  const formatTime = (n, minDigit = 2) =>
+    n.toLocaleString("en-US", { minimumIntegerDigits: minDigit, useGrouping: false });
     
   return (
     <div id="progressBar">
-      <div id="bar">{formatTime(min)}:{formatTime(sec)}</div>
+      <div id="bar">{formatTime(min)}:{formatTime(sec)}:{formatTime(milliSec)}</div>
     </div>
   );
 };
